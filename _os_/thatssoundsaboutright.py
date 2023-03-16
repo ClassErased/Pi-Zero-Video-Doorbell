d17f435f580a0b572472d3b32bf699859fce322d
Aaron <91396239+ClassErased@users.noreply.github.com>
2023-03-16 20:53:21 +0000

    commit message

--- _os_/thatssoundsaboutright.py
+++ _os_/thatssoundsaboutright.py
@@ -5,6 +5,7 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from time import sleep
+import glob
 
 sender = ""
 password = ""
@@ -17,3 +18,12 @@ prefix = "video"
 def capture_img():
     if not os.path.exists(dir):
         os.makedirs(dir)
+    
+    files = sorted(glob.glob(os.path.join(dir, prefix + '[0-9][0-9][0-9].jpg'))) # Returns a list of files that matches the path specified in the function argument
+    
+    count = 0
+    if len(files) > 0:
+        count = int(files[-1][-7:-4])+1 # Use the last filename to initialize the count
+
+if __name__ == '__main__':
+    print("hi my name is, my name is, ricka rucka slim shady")
