263d127c1dd429913481345466e5dd3a1a4df569
Aaron <91396239+ClassErased@users.noreply.github.com>
2023-03-19 15:59:15 +0000

    a

--- _os_/setup.sh
+++ _os_/setup.sh
@@ -4,6 +4,7 @@
 #add wpa_supplicant.confg + ssh file to bootfs
 #POWER ON
 #tty x.x.x.x:22
+#login with default pi - raspberry #If you were putting this on the internet you should change this password first
 
 #sudo passwd root
 #vim /etc/ssh/sshd_config
@@ -13,15 +14,18 @@
 #=====> !!!!!!REMEMBER TO UNDO PermitRootLogin CHANGES AFTER A PROPER USER ACCOUNT HAS BEEN CONFIGURED!!!!!! <======
 #Alternaitvely if you don't wish to use root you can use raspberry's sudo privs to setup a user account
 
+#login as root
 #adduser <params>/useradd <username> #Useradd is a perl script which uses the native adduser binary
 #usermod -aG sudo <username>
 #vim /etc/ssh/sshd_config
 ##Port 22 -> Port 2222
 ##PermitRootLogin yes -> PermitRootLogin prohibit-password
 #service ssh restart
+#confirm changes by trying to login as root
 
 
 #Removing traces of pi user from system
-#login to new account
+#tty x.x.x.x:2222
+#login to new user account
 #sudo deluser pi
 #sudo deluser -remove-home pi
